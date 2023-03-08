c56966bdcfd909a3e8c06163903a317ae9a8f929
Lenwr <saidabou.lateam@gmail.com>
2023-03-08 10:34:25 +0100

    config bdd netlify 1

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,13 +1,15 @@
+const path = require('path')
+
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: 'sqlite',
     connection: {
-      host: env('PGHOST', '127.0.0.1'),
-      port: env.int('PGPORT', 7608),
-      database: env('PGDATABASE', 'railway'),
-      user: env('PGUSER', 'postgres'),
-      password: env('PGPASSWORD', 'WT38GNTv21H5ncp43qB4'),
-      ssl: env.bool(true),
+      filename: path.join(
+        __dirname,
+        '..',
+        env('DATABASE_FILENAME', '.tmp/data.db'),
+      ),
     },
+    useNullAsDefault: true,
   },
 })
