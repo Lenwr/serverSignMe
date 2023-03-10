89ec1cb301e00175e874b3bc65d08b2d9fcaecd7
Lenwr <saidabou.lateam@gmail.com>
2023-03-10 17:47:09 +0100

    config netlfy/strapi

--- config/plugins.js
+++ config/plugins.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   'netlify-deployments': {
     enabled: true,
     config: {
-      accessToken: 'HKl_kFlUhql2zekWOxSyWntfPrDLP7OB5UzyYvDHixY',
+      accessToken: process.env.NETLIFY_DEPLOYMENTS_PLUGIN_ACCESS_TOKEN,
       sites: [
         {
           name: 'serversignme',
-          id: '45df245b-fa84-4136-a3dc-993145f2951a',
+          id: "45df245b-fa84-4136-a3dc-993145f2951a",
           buildHook:
             'https://api.netlify.com/build_hooks/6409e781a061970d7696e528',
           branch: 'main', // optional
